eb7c4729dd70fee3e597512a37d392a970194807
Anna Kaźmierczak <anna.kazmierczak.it@gmail.com>
2019-01-14 21:15:21 +0100

    feat(skicams): add skicams view

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(function ($routeProvider) {
 	$routeProvider
 
-
 		.when('/', {
 			templateUrl: 'views/about.html',
 			controller: 'mainController'
@@ -19,7 +18,6 @@ myApp.config(function ($routeProvider) {
 		.otherwise({
 			redirectTo: '/'
 		});
-
 
 });
 
@@ -45,6 +43,43 @@ myApp.controller('contactController', function ($scope) {
 	};
 
 });
-myApp.controller('skicamsController', function ($scope) {
-	$scope.message = 'skicam page';
+myApp.controller('skicamsController', function ($scope, $http) {
+
+	$scope.currentDate = new Date();
+	$scope.getSkiCams = function () {
+		$http({
+			method: 'GET',
+			url: 'https://makevoid-skicams.p.mashape.com/cams.json',
+			headers: {
+				'X-Mashape-Key': 'kxSXmUymofmshFHhhKxWOSJpqJsJp1I3zNnjsnqKwhITAiC1zw'
+			}
+		}).then(function (res) {
+			$scope.skiCams = res.data;
+			for (var key in $scope.skiCams) {
+				if ($scope.skiCams[key].name === 'Brusson') {
+					$scope.brussonCams = $scope.skiCams[key].cams;
+					if (Object.keys($scope.brussonCams).length > 1) {
+						$scope.brussonImgs = [];
+						for (var i = 0; i < 2; i++) {
+							$scope.brussonImgs.push($scope.brussonCams[Object.keys($scope.brussonCams)[i]].url)
+						}
+					}
+
+				} else if ($scope.skiCams[key].name === 'Piani di Bobbio') {
+					$scope.pianiDiBobbioCams = $scope.skiCams[key].cams;
+					if (Object.keys($scope.pianiDiBobbioCams).length > 1) {
+						$scope.pianiDiBobbioImgs = [];
+						for (var i = 0; i < 2; i++) {
+							$scope.pianiDiBobbioImgs.push(
+								$scope.pianiDiBobbioCams[Object.keys($scope.pianiDiBobbioCams)[i]].url)
+						}
+					}
+				}
+			}
+		}, function (rej) {
+			alert('There is a problem with displaying the cameras. Please try again later.')
+			console.error('Error: ' + rej.data.message)
+		});
+	};
+	$scope.getSkiCams();
 });
